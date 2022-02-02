d6e19281cd878c9512c3754a6a01cc348819b3ab
Tanay Pant <tanaypant@protonmail.com>
2022-02-02 17:53:53 +0100

    Updates from Deepa

--- services/minio-handler.js
+++ services/minio-handler.js
@@ -6,7 +6,8 @@ var minioClient = new Minio.Client({
     endPoint: 'play.minio.io',
     port: 9000,
     accessKey: 'Q3AM3UQ867SPQQA43P2F',
-    secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
+    secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
+    useSSL: true
 });
   
 var minioBucket = 'okta-commerce'
