f551289c75e15e8a7ee400aca7cec115f4e3b6c6
Tanay Pant <tanaypant@protonmail.com>
2022-02-25 16:20:24 +0100

    Revert "Updates from Deepa"
    This reverts commit d6e19281cd878c9512c3754a6a01cc348819b3ab.

--- services/minio-handler.js
+++ services/minio-handler.js
@@ -6,8 +6,7 @@ var minioClient = new Minio.Client({
     endPoint: 'play.minio.io',
     port: 9000,
     accessKey: 'Q3AM3UQ867SPQQA43P2F',
-    secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
-    useSSL: true
+    secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
 });
   
 var minioBucket = 'okta-commerce'
